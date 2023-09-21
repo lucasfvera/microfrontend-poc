0531562bf5e5c361e4e3c2762cde8fa34c1ccff7
lucasfv <lucas@correlation-one.com>
2023-09-21 13:58:16 -0300

    Add internal routing to Home app
    This commit implements routing in the Home app. This routing lives in
    the Home app (not the Container) which allows to build internal pages
    in that external app.

--- home_app/src/App.tsx
+++ home_app/src/App.tsx
@@ -1,22 +1,44 @@
 import { useState } from 'react';
 import './App.css';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
 const Home = () => {
+	return (
+		<>
+			<BrowserRouter>
+				<Routes>
+					<Route index element={<Counter />} />
+					<Route path="/about-counter" element={<AboutCounter />} />
+				</Routes>
+			</BrowserRouter>
+		</>
+	);
+};
+
+const Counter = () => {
 	const [count, setCount] = useState(0);
 
 	return (
+		<div className="card">
+			<button
+				className="primary-button"
+				onClick={() => setCount((count) => count + 1)}
+			>
+				count is {count}
+			</button>
+			<p>
+				Edit <code>src/App.tsx</code> and save to test HMR
+			</p>
+			<Link to="/about-counter">Read about the counter</Link>
+		</div>
+	);
+};
+
+const AboutCounter = () => {
+	return (
 		<>
-			<div className="card">
-				<button
-					className="primary-button"
-					onClick={() => setCount((count) => count + 1)}
-				>
-					count is {count}
-				</button>
-				<p>
-					Edit <code>src/App.tsx</code> and save to test HMR
-				</p>
-			</div>
+			<div>It's a simple MFE implementing a counter</div>
+			<Link to="/">Go back to the counter</Link>
 		</>
 	);
 };
